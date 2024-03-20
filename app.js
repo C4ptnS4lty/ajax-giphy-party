2a5ae4913d9d66af987940ea6eec9c4c2e353c67
C4ptnS4lty <jonlahmann@gmail.com>
2024-03-20 18:10:34 -0400

    Completed

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log("Let's get this party started!");
+const gifZone = $("#gif-zone");
+const input = $("#search");
+
+function addGif(item) {
+    let amount = item.data.length;
+
+    if(amount) {
+        let randIndex = Math.floor(Math.random() * amount);
+        let addCol = $("<div>", { class: "col-md-4 col-12 mb-4"});
+
+        let newImage = $("<img>", {
+            src: item.data[randIndex].images.original.url,
+            class: "w-100"
+        });
+
+        addCol.append(newImage);
+        gifZone.append(addCol);
+    }
+}
+$("form").on("submit", async function(evt) {
+    evt.preventDefault();
+
+    let search = input.val();
+
+    input.val("");
+
+    const retrieve = await axios.get("http://api.giphy.com/v1/gifs/search", {
+        params: {
+            q: search,
+            api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+        }
+    });
+
+    addGif(retrieve.data);
+})
+
+$("#remove").on("click", function() {
+    gifZone.empty();
+  });
